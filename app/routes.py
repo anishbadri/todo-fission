01b52b774b870b2396e8ae28ecf8b60bcd40523f
anishbadri@gmail.com <anishbadri@gmail.com>
2019-06-18 17:37:38 +0530

    to dolist basic completed

--- app/routes.py
+++ app/routes.py
@@ -3,41 +3,44 @@ from app import app, db
 from app.forms import AddToDo
 from app.models import Todo
 
-class todo:
-    def __init__(self, task, time = None):
-        self.task = task
-        self.time = time
-
 # list=[]
-completed = []
+# completed = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = AddToDo()
     newtaskTime = None
     if form.validate_on_submit():
-        newTask = todo(form.newtodo.data)
+        newTask = form.newtodo.data
         if form.time.data:
             newtaskTime = form.time.data   
         newTodo = Todo(task=newTask, time=newtaskTime)       
         db.session.add(newTodo)  
-        db.session.commmit()
+        db.session.commit()
         return redirect(url_for('index'))
-    list = Todo.query.all()
+    list = Todo.query.filter_by(status=0)
+    completed = Todo.query.filter_by(status=1)
     return render_template('index.html', title='Home', form=form, list=list, completed = completed)
 
 
 @app.route('/markcomplete/<int:id>')
 def markComplete(id):
-    completed.append(list.pop(id))
+    complete = Todo.query.get(id)
+    complete.status = 1
+    db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/markincomplete/<int:id>')
 def markInComplete(id):
-    list.append(completed.pop(id))
+    incomplete = Todo.query.get(id)
+    incomplete.status = 0
+    db.session.commit()
     return redirect(url_for('index'))
     
 
+@app.route('/deleteTodo/<int:id>')
+def deleteTodo(id):
+        Todo.query.get(id)
 
 
 
